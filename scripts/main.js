385ad605f33c8fc4d14aa5c02e14e18ee4a3e607
Juan Manuel Rodriguez <cuenta.manuxs@gmail.com>
2024-08-03 19:52:24 -0300

    mejorando estilos parte uno

--- scripts/main.js
+++ scripts/main.js
@@ -64,17 +64,31 @@ function mostrarPaginacion(paginaActual) {
     const contenedorPaginacion = document.getElementById('pagination');
     contenedorPaginacion.innerHTML = "";
 
-    for (let i = 1; i <= totalPaginas; i++) {
+    const crearItemPaginacion = (pagina, texto, activo = false, deshabilitado = false) => {
         const itemPaginacion = document.createElement('li');
-        itemPaginacion.className = `page-item ${i === paginaActual ? 'active' : ''}`;
-        itemPaginacion.innerHTML = `<a class="page-link" href="#">${i}</a>`;
-        itemPaginacion.addEventListener('click', (e) => {
-            e.preventDefault();
-            mostrarProductos(i);
-        });
+        itemPaginacion.className = `page-item ${activo ? 'active' : ''} ${deshabilitado ? 'disabled' : ''}`;
+        itemPaginacion.innerHTML = `<a class="page-link" href="#">${texto}</a>`;
+        if (!deshabilitado) {
+            itemPaginacion.addEventListener('click', (e) => {
+                e.preventDefault();
+                mostrarProductos(pagina);
+            });
+        }
         contenedorPaginacion.appendChild(itemPaginacion);
+    };
+
+    // Flecha para retroceder
+    crearItemPaginacion(paginaActual - 1, '&laquo;', false, paginaActual === 1);
+
+    // Números de paginación
+    for (let i = 1; i <= totalPaginas; i++) {
+        crearItemPaginacion(i, i, i === paginaActual);
     }
+
+    // Flecha para avanzar
+    crearItemPaginacion(paginaActual + 1, '&raquo;', false, paginaActual === totalPaginas);
 }
+
 
 function agregarAlCarrito(index) {
     let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
